0a9336cd9fc17ed08f0f26c80df7937cb87ff44a
Céline Cottier <cottier.celine@gmail.com>
2020-03-24 12:18:17 +0100

    fonction edited

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,25 @@
-const fizzBuzz = () => {
+const fizzBuzz = (nombre) => {
   // Start here
+  let multiple3 = (nombre % 3 ===0);
+  let multiple5 = (nombre % 5===0);
+
+  if(multiple3 && multiple5 ){
+  return "fizzbuzz"
+} 
+else if (multiple5){
+  return "buzz";
+} else if (multiple3){
+  return "fizz";
+} else {
+  return nombre
+}
+ 
 };
+
+
+
+//console.log(fizzBuzz(7));
+
+
 
 module.exports = fizzBuzz;
